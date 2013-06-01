c4f4f9184bdd85f7ac12fe4f55b1c37ae1d5c009
Tomoya Koyanagi <tomk79@gmail.com>
2013-06-01 23:08:28 +0900

    ダウンロードファイルの内容が、それなりにサイトマップぽい感じに一応なった。
    - しかし、CSVと同じ構造をそのまま出力しているにすぎないので、もっと人間が扱いやすい形式にしてく予定。

--- sitemapExcel/register/pxcommand.php
+++ sitemapExcel/register/pxcommand.php
@@ -97,9 +97,61 @@ class pxplugin_sitemapExcel_register_pxcommand extends px_bases_pxcommand{
 		$objPHPExcel->setActiveSheetIndex(0);
 		$objSheet = $objPHPExcel->getActiveSheet();
 
+		// フォント
+		$objSheet->getDefaultStyle()->getFont()->setName('メイリオ');
 
-		$objSheet->getCell('A1')->setValue('ただいま開発中です。');
+		// フォントサイズ
+		$objSheet->getDefaultStyle()->getFont()->setSize(12);
 
+		// 背景色指定(準備)
+		$objSheet->getDefaultStyle()->getFill()->setFillType(PHPExcel_Style_Fill::FILL_SOLID);
+
+		// ウィンドウ枠を固定
+		$objSheet->freezePane('B4');
+
+		// 罫線の一括指定
+		$cell_style_boarder = array(
+		  'borders' => array(
+		    'top'     => array('style' => PHPExcel_Style_Border::BORDER_THIN),
+		    'bottom'  => array('style' => PHPExcel_Style_Border::BORDER_THIN),
+		    'left'    => array('style' => PHPExcel_Style_Border::BORDER_THIN),
+		    'right'   => array('style' => PHPExcel_Style_Border::BORDER_THIN)
+		  )
+		);
+
+		// シートタイトルセル
+		$sheetTitle = '「'.$this->px->get_conf('project.name').'」 サイトマップ';
+		$objSheet->setTitle($sheetTitle);
+		$objSheet->getCell('A1')->setValue($sheetTitle);
+		$objSheet->getStyle('A1')->getFont()->setSize(24);
+
+		$objSheet->getCell('A2')->setValue('Exported: '.date('Y-m-d H:i:s'));
+
+		// 定義行
+		$sitemap_definition = $this->px->site()->get_sitemap_definition();
+		$col = 'A';
+		foreach( $sitemap_definition as $def_row ){
+			$cellName = ($col++).'3';
+			$objSheet->getCell($cellName)->setValue($def_row['name']);
+			$objSheet->getStyle($cellName)->getFill()->getStartColor()->setRGB('dddddd');
+
+			// 罫線の一括指定
+			$objSheet->getStyle($cellName)->applyFromArray( $cell_style_boarder );
+		}
+
+		// データ行
+		$row = 4;
+		foreach( $this->px->site()->get_sitemap() as $page_info ){
+			$col = 'A';
+			foreach( $sitemap_definition as $def_row ){
+				$cellName = ($col++).$row;
+				$objSheet->getCell($cellName)->setValue($page_info[$def_row['key']]);
+
+				// 罫線の一括指定
+				$objSheet->getStyle($cellName)->applyFromArray( $cell_style_boarder );
+			}
+			$row ++;
+		}
 
 		$phpExcelHelper->save($objPHPExcel, $path_work_dir.'tmp.xlsx', 'Excel2007');
 
