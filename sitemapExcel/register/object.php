eadb0a5c2f89cfff7059f9a7924417cee19bf9eb
Tomoya Koyanagi <tomk79@gmail.com>
2013-06-10 09:58:22 +0900

    バージョン0.0.1a1 リリース

---
+++ sitemapExcel/register/object.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * PX Plugin "sitemapExcel"
+ * @author Tomoya Koyanagi.
+ */
+class pxplugin_sitemapExcel_register_object{
+	private $px;
+
+	/**
+	 * コンストラクタ
+	 * @param $px = PxFWコアオブジェクト
+	 */
+	public function __construct($px){
+		$this->px = $px;
+	}
+
+}
+
+?>
